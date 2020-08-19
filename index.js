36e4d6b20499edd4a94aca7b28730268359fcbd9
Damian A. Pastorini <damian.pastorini>
2020-08-19 22:58:51 +0200

    - Reldens - Skills

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * Reldens - Skills
+ *
+ */
+
+module.exports = {
+    Skill: require('./lib/skill'),
+    SkillConst: require('./lib/constants'),
+    Attack: require('./lib/types/attack'),
+    Effect: require('./lib/types/effect'),
+    PhysicalAttack: require('./lib/types/physical-attack'),
+    PhysicalEffect: require('./lib/types/physical-effect')
+};
